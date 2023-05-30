7aec52f2738a01468ee0e615fe12576d556c2e9d
Robledo Pazotto <robledo@gmail.com>
2023-05-30 11:07:20 -0700

    Accuracy tracking

--- script.js
+++ script.js
@@ -6,34 +6,39 @@ let words;
 let practiceWords;
 let wordElements;
 let currentWordWrong = false;
+let start;
+let end;
+let numberOfWords;
+let numberOfLetters;
+let wpm;
+let testMistakes;
+
 const inputElement = document.querySelector("#input");
 const textElement = document.querySelector("#text");
 const headerElement = document.querySelector("#header");
 const footerElement = document.querySelector("#footer");
-let start;
-let end;
-let numberOfWords;
-let wpm;
 
 async function getFile(fileURL) {
     let fileContent = await fetch(fileURL);
     fileContent = await fileContent.text();
-    return fileContent.split("\n").sort(() => 0.5 - Math.random());
+    return fileContent.split("\n");
 }
 
 async function init() {
+    testMistakes = 0;
     currentWordIndex = 0;
     inputElement.value = "";
-    allWords = await getFile("words.txt");
+    allWords ??= await getFile("words.txt");
     practiceWords = new Map(JSON.parse(localStorage.getItem("practiceWords")));
+
     words = allWords
+        .sort(() => 0.5 - Math.random())
         .slice(0, 30 - practiceWords.size)
-        .concat(...practiceWords.keys());
+        .concat(...practiceWords.keys())
+        .sort(() => 0.5 - Math.random());
 
-    let numberOfLetters = words.reduce((acc, word) => acc + word.length, 0);
+    numberOfLetters = words.reduce((acc, word) => acc + word.length, 0);
     numberOfWords = numberOfLetters / 5;
-
-    console.log(numberOfWords);
     textElement.innerHTML = "";
 
     words.forEach((word) => {
@@ -58,77 +63,12 @@ inputElement.addEventListener("input", async (e) => {
             wordElements[currentWordIndex].classList.add("wrong");
             currentWordWrong = true;
         } else {
-            if (currentWordWrong) {
-                const currentWord = words[currentWordIndex];
-                let mistakes = practiceWords.get(currentWord) ?? 0;
-                practiceWords.set(currentWord, (mistakes += 2));
-
-                localStorage.setItem(
-                    "practiceWords",
-                    JSON.stringify([...practiceWords])
-                );
-            } else {
-                let mistakes = practiceWords.get(words[currentWordIndex]) ?? 0;
-                if (mistakes > 0) {
-                    practiceWords.set(words[currentWordIndex], --mistakes);
-                    localStorage.setItem(
-                        "practiceWords",
-                        JSON.stringify([...practiceWords])
-                    );
-                }
-                if (mistakes === 0) {
-                    practiceWords.delete(words[currentWordIndex]);
-                    localStorage.setItem(
-                        "practiceWords",
-                        JSON.stringify([...practiceWords])
-                    );
-                }
-            }
-
-            if (currentWordIndex === words.length - 1) {
-                end = performance.now();
-                wpm = Math.round((numberOfWords / (end - start)) * 60000);
-                footerElement.innerText = `You typed ${wpm} words per minute`;
-                await init();
-            } else {
-                if (currentWordWrong) {
-                    wordElements[currentWordIndex].classList.add("past-wrong");
-                }
-                wordElements[currentWordIndex].classList.add("complete");
-                inputElement.value = "";
-                wordElements[currentWordIndex].classList.remove("current");
-                currentWordIndex++;
-                wordElements[currentWordIndex].classList.add("current");
-                inputElement.attributes.placeholder.value = `Type the word '${words[currentWordIndex]}'`;
-            }
-
+            checkCurrentWord();
+            await jumpToNextWord();
             currentWordWrong = false;
         }
     } else {
-        if (currentWordIndex === 0 && inputElement.value.length === 1) {
-            start = performance.now();
-            footerElement.innerText = "";
-        }
-
-        wordElements[currentWordIndex].classList.add("current");
-        const currentWord = words[currentWordIndex];
-        const currentInput = inputElement.value;
-        const isCorrect = currentWord.startsWith(currentInput);
-        if (isCorrect) {
-            wordElements[currentWordIndex].classList.remove("wrong");
-            inputElement.classList.remove("wrong");
-        } else {
-            wordElements[currentWordIndex].classList.remove("complete");
-            wordElements[currentWordIndex].classList.add("wrong");
-            inputElement.classList.add("wrong");
-
-            currentWordWrong = true;
-        }
-
-        if (inputElement.value === "") {
-            wordElements[currentWordIndex].classList.remove("complete");
-            wordElements[currentWordIndex].classList.remove("wrong");
-        }
+        checkInput(e.data);
     }
     setHeader();
 });
@@ -136,6 +76,81 @@ inputElement.addEventListener("input", async (e) => {
 document.querySelector("body").addEventListener("keydown", (e) => {
     inputElement.focus();
 });
+
+function checkInput(key) {
+    if (currentWordIndex === 0 && inputElement.value.length === 1) {
+        start = performance.now();
+        footerElement.innerText = "";
+    }
+
+    wordElements[currentWordIndex].classList.add("current");
+    const currentWord = words[currentWordIndex];
+    const currentInput = inputElement.value;
+    const isCorrect = currentWord.startsWith(currentInput);
+    if (isCorrect) {
+        wordElements[currentWordIndex].classList.remove("wrong");
+        inputElement.classList.remove("wrong");
+    } else {
+        wordElements[currentWordIndex].classList.remove("complete");
+        wordElements[currentWordIndex].classList.add("wrong");
+        inputElement.classList.add("wrong");
+
+        currentWordWrong = true;
+        if (key !== "Backspace") {
+            testMistakes++;
+        }
+    }
+
+    if (inputElement.value === "") {
+        wordElements[currentWordIndex].classList.remove("complete");
+        wordElements[currentWordIndex].classList.remove("wrong");
+    }
+}
+
+async function jumpToNextWord() {
+    if (currentWordIndex === words.length - 1) {
+        end = performance.now();
+        wpm = Math.round((numberOfWords / (end - start)) * 60000);
+        let accuracy = Math.round(100 - (testMistakes / numberOfLetters) * 100);
+        footerElement.innerText = `You typed ${wpm} words per minute with ${accuracy}% accuracy`;
+        console.log(
+            `Number of letters: ${numberOfLetters}, `,
+            `Number of mistakes: ${testMistakes}`
+        );
+        await init();
+    } else {
+        if (currentWordWrong) {
+            wordElements[currentWordIndex].classList.add("past-wrong");
+        }
+        wordElements[currentWordIndex].classList.add("complete");
+        inputElement.value = "";
+        wordElements[currentWordIndex].classList.remove("current");
+        currentWordIndex++;
+        wordElements[currentWordIndex].classList.add("current");
+        inputElement.attributes.placeholder.value = `Type the word '${words[currentWordIndex]}'`;
+    }
+}
+
+function checkCurrentWord() {
+    if (currentWordWrong) {
+        const currentWord = words[currentWordIndex];
+        let mistakes = practiceWords.get(currentWord) ?? 0;
+        practiceWords.set(currentWord, (mistakes += 2));
+    } else {
+        let mistakes = practiceWords.get(words[currentWordIndex]) ?? 0;
+        if (mistakes > 1) {
+            practiceWords.set(words[currentWordIndex], --mistakes);
+        } else {
+            practiceWords.delete(words[currentWordIndex]);
+        }
+    }
+
+    save();
+}
+
+function save() {
+    localStorage.setItem("practiceWords", JSON.stringify([...practiceWords]));
+}
 
 function setHeader() {
     if (practiceWords.size > 0) {
